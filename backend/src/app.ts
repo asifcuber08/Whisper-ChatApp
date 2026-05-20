d524f3f9046935bda6f1319505c7b541c363d236
Asif Shamim <asifshamim12816@gmail.com>
2026-05-20 17:20:55 +0530

    feat: update dependencies and integrate Clerk for authentication
    - Updated package.json to include new dependencies: @clerk/clerk-react, @tanstack/react-query, axios, lucide-react, react-router, socket.io-client, tailwindcss, and zustand.
    - Removed unused dependencies and updated devDependencies to compatible versions.
    - Refactored App.jsx to implement Clerk authentication with SignedIn and SignedOut components.
    - Added ClerkProvider in main.jsx to wrap the App component with the necessary authentication context.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
 import express from "express";
+import { clerkMiddleware } from "@clerk/express";
 
-import authRoutes from "./routes/authRoutes"
-import chatRoutes from "./routes/chatRoutes"
-import messageRoutes from "./routes/messageRoutes"
-import userRoutes from "./routes/userRoutes"
+import authRoutes from "./routes/authRoutes";
+import chatRoutes from "./routes/chatRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import userRoutes from "./routes/userRoutes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
-app. use(express.json()) // parse incoming JSON request bodies and makes then available as req.body in your route handlers.
+app.use(express.json()); // parse incoming JSON request bodies and makes then available as req.body in your route handlers.
+
+app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
   res.json({ status: "ok", message: "Server is running" });
@@ -18,5 +22,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// error handlers must come after all the routes and other middlewares so they can catch errors passed with next(err) or thrown inside async handlers.
+app.use(errorHandler);
 
 export default app;
