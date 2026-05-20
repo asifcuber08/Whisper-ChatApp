641a3080fa679a817c74478b8637ad3e4815eb90
Asif Shamim <asifshamim12816@gmail.com>
2026-05-20 18:31:16 +0530

    feat: implement chat and message retrieval, and user listing endpoints

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import {
+  getChats,
+  getOrCreateChat,
+} from "../controllers/chatController copy 3";
 
 const router = Router();
+
+router.use(protectRoute);
+
+router.get("/", getChats);
+router.post("/with/:participantId", getOrCreateChat);
 
 export default router;
