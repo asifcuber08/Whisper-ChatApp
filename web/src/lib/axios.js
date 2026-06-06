eab3d57c71751578c4c255d8e99b2bbd7bc79240
Asif Shamim <asifshamim12816@gmail.com>
2026-06-06 21:32:35 +0530

    feat: implement chat functionality with real-time messaging, user presence, and chat management

--- web/src/lib/axios.js
+++ web/src/lib/axios.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-// const apiUrl = import.meta.env.VITE_API_URL;
-// if (!apiUrl) {
-//   console.error("VITE_API_URL environment variable is not set");
-// }
+const apiUrl = import.meta.env.VITE_API_URL;
+if (!apiUrl) {
+  console.error("VITE_API_URL environment variable is not set");
+}
 
 const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL + "/api",
-  baseURL: "https://whisper-chatapp-8lan0.sevalla.app/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
